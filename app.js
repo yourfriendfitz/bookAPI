4b5b1856a36091a8bc89b5c189e0c8bc686f89e4
Joshua Fitzgerald <fitzgerald913@gmail.com>
2019-09-05 14:01:32 -0500

    fixed route

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.post("/books/add", async (req, res, next) => {
   }
 });
 
-app.post("/books/delete/:id", async (req, res, next) => {
+app.delete("/books/delete/:id", async (req, res, next) => {
   const id = req.params.id;
   const deleteBook = await models.Book.destroy({
     where: {
