5bcb876369633c9bf7b1ee0f45385e6900e3921f
Joshua Fitzgerald <fitzgerald913@gmail.com>
2019-09-16 17:21:13 -0500

    added login route to test

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const models = require("./models");
+const jwt = require("jsonwebtoken");
 const cors = require("cors");
 const app = express();
 const randomCoordinates = require("random-coordinates");
@@ -7,6 +8,20 @@ const randomCoordinates = require("random-coordinates");
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+const authenticate = (req, res, next) => {
+  if (req.headers.authorization) {
+    const authHeader = req.headers.authorization;
+    const token = authHeader.split(" ")[1];
+    const decoded = jwt.verify(token, "privateKey");
+    const username = decoded.username;
+    const authUser = users.find(user => user.username === username);
+    authUser ? next() : res.json({ error: "unauthorized" });
+    next();
+  } else {
+    res.json({ error: "headers missing" });
+  }
+};
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
@@ -56,6 +71,25 @@ app.get("/coordinates", (req, res) => {
     coordinatesArray = [...coordinatesArray, coordinate];
   }
   res.json(coordinatesArray);
+});
+
+// book login JWT
+app.post("/login", function(req, res) {
+  const users = [{ username: "fitz", password: "password" }]; // for testing
+  const username = req.body.username;
+  const password = req.body.password;
+
+  const persistedUser = users.find(
+    u => u.username == username && u.password == password
+  );
+
+  if (persistedUser) {
+    const token = jwt.sign({ username }, "privateKey");
+    console.log(token);
+    res.json({ token });
+  } else {
+    res.send("invalid");
+  }
 });
 
 function normalizePort(val) {
